541e98ade4841481126d32c99511768033fe94be
Tomas <Aquista35@gmail.com>
2021-04-24 17:20:56 -0300

    Agregada la primera arma

---
+++ TP05 - AquistapaceTomas/Assets/Scrips/Player/ChangeWeapon.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeWeapon : MonoBehaviour
+{
+    public GameObject Weapon_1;
+    public GameObject Weapon_2;
+
+    void Start()
+    {
+        Weapon_1.SetActive(true);
+        Weapon_2.SetActive(false);
+    }
+
+    void Update()
+    {
+        ChangeWeapons();
+    }
+
+    void ChangeWeapons()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            Weapon_2.SetActive(false);
+            Weapon_1.SetActive(true);
+        }
+        if (Input.GetKeyDown("2"))
+        {
+            Weapon_1.SetActive(false);
+            Weapon_2.SetActive(true);
+        }
+    }
+}
---
+++ TP05 - AquistapaceTomas/Assets/Scrips/Player/ShootingWeapon.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingWeapon : MonoBehaviour
+{
+    public Transform rayWeapon;
+    public float range = 100f;
+
+    public float damage = 10f;
+
+    void Update()
+    {
+        Shooting();
+    }
+
+    void Shooting()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(rayWeapon.position, rayWeapon.forward, out hit, range))
+            {
+                Target target = hit.transform.GetComponent<Target>();
+
+                if (hit.transform.GetComponent<Target>() != null)
+                {
+                    target.IsDamaged(damage);
+                    Debug.Log(target.health);
+                }
+            }
+        }
+
+        //Debug.DrawRay(rayWeapon.position, rayWeapon.forward * range, Color.blue);
+    }
+}
