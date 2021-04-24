5066b33a6a4e5055d1287d4c1e8aa2a79ee72ce6
Tomas <Aquista35@gmail.com>
2021-04-24 01:26:37 -0300

    Movimiento del player

---
+++ TP05 - AquistapaceTomas/Assets/Scrips/PlayerMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speedMovement = 10f;
+    public float gravity = -9.8f;
+    public float jumpheight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    void Update()
+    {
+        Gravity();
+        Jump();
+        Movement();
+    }
+
+    // -----------------------------
+
+    void Movement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speedMovement * Time.deltaTime);
+    }
+
+    void Gravity()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpheight * -2f * gravity);
+        }
+    }
+}
---
+++ TP05 - AquistapaceTomas/Assets/Scrips/PlayerShoot.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public GameObject Weapon1;
+    public GameObject Weapon2;
+
+    void Start()
+    {
+        Weapon1.SetActive(true);
+        Weapon2.SetActive(false);
+    }
+
+    void Update()
+    {
+        ChangeWeapon();
+    }
+
+    void ChangeWeapon()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            Weapon2.SetActive(false);
+            Weapon1.SetActive(true);
+        }
+        if (Input.GetKeyDown("2"))
+        {
+            Weapon1.SetActive(false);
+            Weapon2.SetActive(true);
+        }
+    }
+}
